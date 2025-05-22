954f98f679b63600dfaea3efa23b55e892766a7b
eduardoxxmenezes <eduardomenezesdeo@gmail.com>
2025-05-22 17:12:31 -0300

    Atualização 2

---
+++ src/View/js/logIn.js
@@ -0,0 +1,9 @@
+fetch('../json/data.json')
+
+let form = document.querySelector("#formLogin");
+let campoUsuario = document.querySelector("#campoUsuario");
+let campoSenha = document.querySelector("#campoSenha");
+
+function logIn(){
+    
+}
